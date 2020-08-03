0b896da4a20e8ed2ab8a5f622fb9e2f71316afc6
Michael Schunk <Michael.Schunk@online.de>
2020-08-03 19:38:41 +0200

    clean code and added comments

--- jails-overview.py
+++ jails-overview.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
+
+'''
+--- Jails Overview ---
+This script gives a compact overview about your fail2ban jails, espacially if you have a lot of jails.
+This script is tested on Centos7 and Ubuntu 18.04.
+Its possible that the output of fail2ban slightly varies between linux distributions and thevscript fail.
+
+
+'''
+
+
 import subprocess
-#from collections import namedtuple
 from tabulate import tabulate
 
 # create emtpy arrays
@@ -12,19 +22,14 @@ totalbanned = []
 bannediplist = []
 bannediplistperjail =[]
     
-#with open("./jails.txt") as jails:
-#    content = jails.read()
-#    jaillist = content.split()
-#    jails.close()
-
+# get aktive jails
 jaillist = subprocess.check_output('fail2ban-client status', shell=True)
 jaillist = jaillist.replace(',','') #remove "," from output
 jaillist = jaillist.split() #seperate output to list elements
 jaillist = jaillist[9:] # drop first 9 elements
-#print(jaillist)
-#exit(0)    
 
-#check if system is centos and uses systemd logging for sshd
+
+#check if system is centos and uses journal logging for sshd (longer output)
 os = subprocess.check_output('cat /proc/version', shell=True)
 
 
@@ -32,10 +37,8 @@ for jail in jaillist:
 
 
    jailcontent = subprocess.check_output('fail2ban-client status {}'.format(jail), shell=True)
-   #jailcontent = os.popen('fail2ban-client status {}'.format(jail)).read()	   
-   #jailcontent = os.system('fail2ban-client status {}'.format(jail))
    jailcontent = jailcontent.split()
-   #print(jailcontent)
+   
    if jail == 'sshd' and 'centos' in os:
       jailname.append(jailcontent[4])
       currentfailed.append(jailcontent[11])
@@ -43,19 +46,17 @@ for jail in jaillist:
       currentlybanned.append(jailcontent[29])
       totalbanned.append(jailcontent[33])
       #check if last element is empty or is "list" otherwise blocked ips are given
-     # lets get the list of ips if ips are present
+      #lets get the list of ips if ips are present
       if jailcontent[-1] != 'None' and jailcontent[-1] != 'list:': #if last element in array not ..
-         count = len(jailcontent) #get number of entries in array
-
+         count = len(jailcontent)   #get number of entries in array
          for i in range(36, count): #loop from element 36 ( first ip if they are present) to last element of array
             bannediplistperjail.append(jailcontent[i])
-
          bannediplist.append(bannediplistperjail) #array in array because in bannediplist can be multiple ips..
       else:
          bannediplist.append('')
-       	 #print (bannediplist)
+       	
 
-   elif("apache" in jail):
+   elif("apache" in jail): # longer output because of 2 logfiles
       jailname.append(jailcontent[4])
       currentfailed.append(jailcontent[11])
       totalfailed.append(jailcontent[16])
@@ -69,8 +70,8 @@ for jail in jaillist:
          bannediplist.append(bannediplistperjail)
       else:
          bannediplist.append('')
-       	 #print (bannediplist)
-   else:
+       	 
+   else: # standard output (1 logfile)
       jailname.append(jailcontent[4])
       currentfailed.append(jailcontent[11])
       totalfailed.append(jailcontent[16])
@@ -84,19 +85,9 @@ for jail in jaillist:
          bannediplist.append(bannediplistperjail)
       else:
          bannediplist.append('')
-         #print (bannediplist)    
-   
-   #print (jailcontent[4]) #jail-name
-   #print (jailcontent[11]) #currently-failed
-   #print (jailcontent[16]) #total failed
-   #print (jailcontent[27]) #currently banned
-   #print (jailcontent[31]) #total banned
-   #print (jailcontent[36]) #ip list if banned ips are present
-
-#print(bannediplist)
-
+        
+# create table
 headers = ['Jail', 'Current Failed', 'Total Failed', 'Current Banned', 'Total Banned', 'Banned Ips'] 
 table = zip(jailname, currentfailed, totalfailed , currentlybanned, totalbanned, bannediplist)
-#print(table)
 x = (tabulate(table, headers=headers,tablefmt="fancy_grid"))
 print (x)
